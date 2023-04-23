dbf17a1badb591198b9e806b5ed1154edc3c18d0
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-04-23 20:05:25 +0530

    cors

--- cors/index.js
+++ cors/index.js
@@ -1,7 +1,7 @@
 const cors = require('cors')
 
 const corsOption ={
-    origin:['http://localhost:3000'],
+    origin:['http://localhost:3000', 'https://oruphones.netlify.app/'],
     optionsSuccessStatus:200
 }
 
